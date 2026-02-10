df9e2bea27ed234ae886cf4005a576bedca5982c
princetritechy-dev <prince.tritechy@gmail.com>
2026-02-10 23:38:00 +0530

    Added new folder: new-folder-name

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
   trailingSlash: true,
+
+  // ✅ your folder name
+  basePath: "/taj-virtual",
+  assetPrefix: "/taj-virtual",
 };
 
 export default nextConfig;
