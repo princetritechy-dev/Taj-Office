cb82945f20038670a9bb23aa0377e05cddeb12d1
princetritechy-dev <prince.tritechy@gmail.com>
2026-02-11 14:22:56 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,7 @@ import { useMemo, useState } from "react";
 type FAQ = { q: string; a: string };
 import Header from "../components/header";
 import Footer from "../components/footer";
-import CSS from "../globals.css";
+import "../globals.css";
 export default function About() {
     return (
         <main>
