31ef8a547b6babb97c1fd50ae6a318899e419bed
princetritechy-dev <prince.tritechy@gmail.com>
2026-02-11 15:23:08 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
   images: { unoptimized: true },
   trailingSlash: true,
-
-  // ✅ your folder name
-  basePath: "/taj-virtual",
-  assetPrefix: "/taj-virtual",
 };
 
 export default nextConfig;
