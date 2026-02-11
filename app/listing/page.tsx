b56f575130c225abfb66af48c5fedaa3461de5dc
princetritechy-dev <prince.tritechy@gmail.com>
2026-02-11 14:59:04 +0530

    Update page.tsx

--- app/listing/page.tsx
+++ app/listing/page.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import Image from "next/image";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import "../globals.css";
+import Header from "../components/header";
+import Footer from "../components/footer";
 
 export default function Listing() {
   return (
