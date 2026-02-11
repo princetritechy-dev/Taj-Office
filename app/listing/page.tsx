8b95b0621693bde9193fda7b0c0fbd945ae341b5
princetritechy-dev <prince.tritechy@gmail.com>
2026-02-11 14:54:13 +0530

    Update page.tsx

--- app/listing/page.tsx
+++ app/listing/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { useMemo, useState } from "react";
 import Header from "../components/header";
 import Footer from "../components/footer";
+import "../globals.css";
 export default function Listing() {
     return (
         <main>
