a7c3abcf6f7ce95ae5f97c466a137311594d28ef
princetritechy-dev <prince.tritechy@gmail.com>
2026-02-17 01:09:21 +0530

    Update header.tsx

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,35 +1,29 @@
-import Link from "next/link"
-import Image from "next/image"
-export default function Header(){
-  return(
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Header() {
+  return (
     <header className="nav">
-        <div className="container navInner">
-          <div className="brand">
-            
-            {/* Logo Image */}
-            <a href="/">
-            <Image
-              src="/images/logo.png"
-              alt="Virtual Office Anywhere Logo"
-              width={150}
-              height={50}
-              className="brandLogo"
-            />
-              </a>
-          </div>
+      <div className="container navInner">
+        <Link href="/" className="brand">
+          <Image
+            src="/images/logo.png"
+            alt="Virtual Office Anywhere Logo"
+            width={150}
+            height={50}
+            className="brandLogo"
+            priority
+          />
+        </Link>
 
-
-          <nav className="navLinks" aria-label="Primary">
-            <Link href="#">Home</Link>
-            <Link href="/about-us">About Us</Link>
-            <Link href="/why-work-with-us">Why work with us</Link>
-            <Link href="/Listing">Locations</Link>
-            <Link href="/contact-us">Contact Us</Link>
-            {/* <Link className="navLogin" href="#">
-              Login
-            </Link> */}
-          </nav>
-        </div>
-      </header>
-  )
+        <nav className="navLinks" aria-label="Primary">
+          <Link href="/">Home</Link>
+          <Link href="/about-us">About Us</Link>
+          <Link href="/why-work-with-us">Why work with us</Link>
+          <Link href="/listing">Locations</Link>
+          <Link href="/contact-us">Contact Us</Link>
+        </nav>
+      </div>
+    </header>
+  );
 }
