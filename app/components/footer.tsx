ffe9f45ec438e423e6a37288c41e01dc2249b9da
princetritechy-dev <prince.tritechy@gmail.com>
2026-02-18 14:12:41 +0530

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,22 +1,32 @@
 import Link from "next/link";
-import Image from "next/image";
 import Script from "next/script";
+import Image from "next/image";
 export default function Footer(){
   return(
-    <footer className="footer">
-            <div className="footerBrand">
-              <div className="brandText">
-                <div className="brand">
-                <img
-                  src="/images/logo2.png" 
-                  alt="Virtual Office Anywhere"
-                  className="brandLogo"
-                />
-              </div>
+    <><footer className="footer">
+      <div className="footerBrand">
+        <div className="brandText">
+          <div className="brand">
+            <img
+              src="/images/logo2.png"
+              alt="Virtual Office Anywhere"
+              className="brandLogo" />
+          </div>
 
-              </div>
-            </div>
-            <div className="footerCopy">© 2025 Virtual Office Anywhere. All rights reserved.</div>
-          </footer>
+        </div>
+      </div>
+      <div className="footerCopy">© 2025 Virtual Office Anywhere. All rights reserved.</div>
+    </footer><Script id="hamburger-script" strategy="afterInteractive">
+        {`
+          const navWrapper = document.querySelector('.navWrapper');
+          const hamburger = document.querySelector('.hamburger');
+
+          if (hamburger) {
+            hamburger.addEventListener('click', () => {
+              navWrapper.classList.toggle('open');
+            });
+          }
+        `}
+      </Script></>
   )
 }
